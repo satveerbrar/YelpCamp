02cf46449b37d7abceed5e5a7d03f189dd532bc2
Satveer Brar <satveerbrar180@gmail.com>
2021-12-29 02:33:00 -0500

    public directory for static files

--- index.js
+++ index.js
@@ -2,12 +2,8 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const { campgroundSchema, reviewSchema } = require('./validationSchema');
-const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
-const Campground = require('./models/campground');
-const Review = require('./models/review');
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
 
@@ -21,16 +17,12 @@ try {
 const app = express();
 
 app.engine('ejs', ejsMate)  // to use boilerplate for ejs files
-
-app.use(express.static(path.join(__dirname, '/public')));
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
-
-
-
-
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
+
+app.use(express.static(path.join(__dirname, '/public'))); // to serve static files from public directories
+app.use(express.urlencoded({ extended: true })); // to decode req.body of POST requests
+app.use(methodOverride('_method')); // to use PUT PATCH DELETE
 
 app.use('/campgrounds', campgrounds);
 app.use('/campgrounds/:id', reviews);
@@ -38,8 +30,6 @@ app.use('/campgrounds/:id', reviews);
 app.get('/', (req, res) => {
     res.render('home')
 })
-
-
 
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404));
@@ -50,7 +40,6 @@ app.use((err, req, res, next) => {
     if (!err.message) err.message = "Oh No, Something went wrong!"
     res.status(statusCode).render("error", { err });
 })
-
 
 app.listen('3000', () => {
     console.log("Serving on port 3000");
